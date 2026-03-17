2bd63eeaf855ca6746e85dd07b11d991b9eeafd3
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-17 21:36:48 +0500

    feat: add static header

--- src/shared/ui/layout.tsx
+++ src/shared/ui/layout.tsx
@@ -1,5 +1,5 @@
 import { useCurrentUser } from '@/entities/user';
-import { Navigate, Outlet } from '@tanstack/react-router';
+import { Link, Navigate, Outlet } from '@tanstack/react-router';
 
 export function Layout() {
   const { user, isPending, error } = useCurrentUser();
@@ -10,7 +10,20 @@ export function Layout() {
   if (!user) return <Navigate to="/signin" replace />;
 
   return (
-    <div className="flex h-full">
+    <div>
+      <header className="flex justify-between py-4 px-32 border-b border-gray-200">
+        <nav className="flex gap-10">
+          <Link to="/">Главная</Link>
+
+          {user.canCreateBoards && <Link to="/board">Доска оценивания</Link>}
+          {user.canViewStats && <Link to="/dashboard">Статистика</Link>}
+          {user.canManageSkills && <Link to="/manage/skills">Навыки</Link>}
+          {user.canManageUsers && <Link to="/manage/users">Пользователи</Link>}
+        </nav>
+
+        <a>Выйти</a>
+      </header>
+
       <Outlet />
     </div>
   );
