a7e662efb684d58ede6faf213f80095c44035f1b
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-19 01:00:26 +0500

    feat: log out logic

--- src/entities/user/api/user.ts
+++ src/entities/user/api/user.ts
@@ -1,18 +1,29 @@
 import type { CurrentUser } from '../model/user';
 
-export async function signin(data: { login: string; password?: string }) {
-  // TODO: send request to the API
-  console.log(data);
-}
+let currentUser: CurrentUser | null = null;
 
-export async function fetchCurrentUser(): Promise<CurrentUser> {
+export async function signIn(data: { login: string; password?: string }) {
   // TODO: send request to the API
-  return {
+  currentUser = {
     id: 1,
-    username: 'username',
+    username: data.login,
     canCreateBoards: true,
     canViewStats: true,
     canManageSkills: true,
     canManageUsers: true,
   };
+}
+
+export async function signOut() {
+  // TODO: send request to the API
+  currentUser = null;
+}
+
+export async function fetchCurrentUser(): Promise<CurrentUser> {
+  // TODO: send request to the API
+  if (currentUser) {
+    return currentUser;
+  } else {
+    throw new Error('Not logged in');
+  }
 }
