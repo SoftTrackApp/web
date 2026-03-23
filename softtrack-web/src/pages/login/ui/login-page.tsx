f66463d8980cb87340d81286ff5945cdbb3590ff
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-24 03:17:11 +0500

    feat: add react-hook-form

--- softtrack-web/src/pages/login/ui/login-page.tsx
+++ softtrack-web/src/pages/login/ui/login-page.tsx
@@ -1,28 +1,55 @@
 import classes from './login-page.module.css';
+import { useForm } from 'react-hook-form';
+
+const defaultValues = {
+  username: '',
+  password: '',
+};
 
 export function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ defaultValues });
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <div className={classes.container}>
       <h1 className={classes.title}>Вход в систему</h1>
 
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={onSubmit}>
         <div className={classes.loginField}>
           <label className={classes.label} htmlFor="username">
             Логин
           </label>
-          <input className={classes.input} type="text" id="username" autoComplete="username" />
+          
+          <input
+            className={classes.input}
+            type="text"
+            id="username"
+            autoComplete="username"
+            {...register('username', { required: 'Введите логин' })}
+          />
+
+          {errors.username && <span className={classes.errorMessage}>{errors.username.message}</span>}
         </div>
 
         <div className={classes.passwordField}>
           <label className={classes.label} htmlFor="password">
             Пароль
           </label>
+
           <input
             className={classes.input}
             type="password"
             id="password"
             autoComplete="current-password"
+            {...register('password', { required: 'Введите пароль' })}
           />
+
+          {errors.password && <span className={classes.errorMessage}>{errors.password.message}</span>}
         </div>
 
         <button className={classes.button} type="submit">
