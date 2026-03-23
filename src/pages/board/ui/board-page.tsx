28a7fa78a59a4cb2ecfd90b0f9338e18fcdcc61e
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-24 01:46:33 +0500

    feat: add tanstack router

---
+++ src/pages/board/ui/board-page.tsx
@@ -0,0 +1,13 @@
+import classes from './board-page.module.css';
+
+export function BoardPage() {
+  return (
+    <div className={classes.container}>
+      <section></section>
+
+      <section></section>
+
+      <section></section>
+    </div>
+  );
+}
