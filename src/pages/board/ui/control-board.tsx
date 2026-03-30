ca9275da0bf48a7080bb0cdbed8d13aac97d8a05
Kirill Siukhin <kirillsiukhin@proton.me>
2026-03-30 23:34:54 +0500

    feat: add user search logic

--- src/pages/board/ui/control-board.tsx
+++ src/pages/board/ui/control-board.tsx
@@ -1,12 +1,24 @@
 import classes from './control-board.module.css';
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Input } from '@/shared/ui';
 import { useAppDispatch, useAppSelector } from '@/app/store';
 import { UserEntity } from '@/entities/user';
 
 export function ControlBoard() {
+  const [searchText, setSearchText] = useState('');
+
   const { users, loading, error } = useAppSelector((state) => state.users);
   const dispatch = useAppDispatch();
+
+  const filteredUsers = useMemo(() => {
+    if (!users) return [];
+
+    const query = searchText.toLowerCase();
+
+    return users.filter(
+      (u) => u.name.toLowerCase().includes(query) || u.surname.toLowerCase().includes(query),
+    );
+  }, [users, searchText]);
 
   useEffect(() => {
     dispatch(UserEntity.actions.fetchUsers());
@@ -16,22 +28,28 @@ export function ControlBoard() {
     <div className={classes.container}>
       <section className={classes.usersSection}>
         <div className={classes.search}>
-          <Input type="text" placeholder="Найти..." role="search" />
+          <Input
+            type="text"
+            placeholder="Найти..."
+            role="search"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+          />
         </div>
 
         {loading ? (
           <span>Loading...</span>
         ) : error ? (
           <span>{error}</span>
-        ) : users ? (
+        ) : (
           <div className={classes.userList}>
-            {users.map((user) => (
+            {filteredUsers.map((user) => (
               <div key={user.id} className={classes.userCard}>
                 {user.name} {user.surname}
               </div>
             ))}
           </div>
-        ) : null}
+        )}
       </section>
     </div>
   );
