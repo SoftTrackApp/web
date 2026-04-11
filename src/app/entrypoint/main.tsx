f17896e86fbb2bac56a12e974efb30fd7aefdc75
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-12 03:15:18 +0500

    chore: add import alias, create /app

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -1,9 +1,9 @@
-import './index.css';
+import '../styles/index.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
-import { routeTree } from './routeTree.gen';
+import { routeTree } from '@/routeTree.gen';
 
 const router = createRouter({ routeTree });
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src'),
+    },
+  },
 });
