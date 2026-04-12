a8372062986a248f91301b052a6ccb6d1c4569a1
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-13 01:51:55 +0500

    chore: configure Redux store

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -3,8 +3,10 @@ import '@fontsource-variable/rubik/wght.css';
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { routeTree } from '@/routeTree.gen';
+import { store } from '../store';
 
 const router = createRouter({ routeTree });
 
@@ -16,6 +18,8 @@ declare module '@tanstack/react-router' {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
