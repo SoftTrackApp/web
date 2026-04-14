bb4b7f62fa243052cb8abc6b4d91bd33a3f540f0
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-15 00:00:36 +0500

    feat: add select for sorting

--- src/pages/dashboard/ui/dashboard-page.tsx
+++ src/pages/dashboard/ui/dashboard-page.tsx
@@ -2,8 +2,16 @@ import classes from './dashboard-page.module.css';
 import { Select } from '@/shared/ui';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserFeature } from '@/features/user';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { SkillCard } from './skill-card';
+
+type Sort = 'alpha' | 'high-score' | 'low-score';
+
+const skills = [
+  { title: 'Коммуникация', rates: 15 },
+  { title: 'Работа в команде', rates: 10 },
+  { title: 'Вербальная коммуникация', rates: 5 },
+];
 
 export function DashboardPage() {
   const dispatch = useDispatch();
@@ -11,8 +19,29 @@ export function DashboardPage() {
   const { users } = useSelector(UserFeature.selectors.selectUsers);
 
   const [userId, setUserId] = useState<number | null>(null);
+  const [sort, setSort] = useState<Sort>('alpha');
 
   const selectedUser = users.find((u) => u.id === userId) || users[0] || null;
+
+  const sortedSkills = useMemo(() => {
+    const copy = [...skills];
+
+    switch (sort) {
+      case 'alpha':
+        copy.sort((a, b) => a.title.localeCompare(b.title));
+        break;
+
+      case 'high-score':
+        copy.sort((a, b) => b.rates - a.rates);
+        break;
+
+      case 'low-score':
+        copy.sort((a, b) => a.rates - b.rates);
+        break;
+    }
+
+    return copy;
+  }, [sort]);
 
   useEffect(() => {
     dispatch(UserFeature.actions.fetchUsers());
@@ -24,23 +53,34 @@ export function DashboardPage() {
     <div className={classes.wrapper}>
       <h1 className={classes.title}>Статистика</h1>
 
-      <Select className={classes.select} onChange={(e) => setUserId(Number(e.target.value))}>
-        {users.map((user) => (
-          <option key={user.id} value={user.id}>
-            {user.surname} {user.name}
-          </option>
-        ))}
-      </Select>
+      <div className={classes.filters}>
+        <Select className={classes.select} onChange={(e) => setUserId(Number(e.target.value))}>
+          {users.map((user) => (
+            <option key={user.id} value={user.id}>
+              {user.surname} {user.name}
+            </option>
+          ))}
+        </Select>
+
+        <Select
+          className={classes.select}
+          value={sort}
+          onChange={(e) => setSort(e.target.value as Sort)}
+        >
+          <option value="alpha">По названию (А-Я)</option>
+          <option value="high-score">Сначала высокие баллы</option>
+          <option value="low-score">Сначала низкие баллы</option>
+        </Select>
+      </div>
 
       <span>
         Выбранный пользователь: {selectedUser.surname} {selectedUser.name}
       </span>
 
       <div className={classes.skills}>
-        <SkillCard title="Коммуникация" rates={15} maxRates={15} />
-        <SkillCard title="Коммуникация" rates={8} maxRates={15} />
-        <SkillCard title="Коммуникация" rates={4} maxRates={15} />
-        <SkillCard title="Коммуникация" rates={2} maxRates={15} />
+        {sortedSkills.map((skill) => (
+          <SkillCard key={skill.title} title={skill.title} rates={skill.rates} maxRates={15} />
+        ))}
       </div>
     </div>
   );
