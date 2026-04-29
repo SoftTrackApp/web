8526750406dfe478fa3592d41fdf2b9bb66fd403
Kirill Siukhin <kirillsiukhin@proton.me>
2026-04-30 01:35:46 +0500

    feat: input and login page redesign

--- src/shared/ui/input.tsx
+++ src/shared/ui/input.tsx
@@ -1,15 +1,37 @@
 import clsx from 'clsx';
 import classes from './input.module.css';
+import { CircleAlert } from 'lucide-react';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
+  trailingIcon?: React.ReactNode;
+  destructive?: boolean;
+  onTrailingIconClick?: () => void;
 }
 
-export function Input({ className, icon, ...props }: InputProps) {
+export function Input({
+  className,
+  icon,
+  trailingIcon,
+  destructive = false,
+  onTrailingIconClick,
+  ...props
+}: InputProps) {
   return (
-    <label className={clsx(classes.wrapper, className)}>
+    <label className={clsx(classes.wrapper, destructive && classes.destructive, className)}>
       {icon && <div className={classes.icon}>{icon}</div>}
+
       <input className={classes.input} {...props} />
+
+      {trailingIcon ? (
+        <div className={classes.trailingIcon} onClick={onTrailingIconClick}>
+          {trailingIcon}
+        </div>
+      ) : destructive ? (
+        <div className={clsx(classes.trailingIcon, classes.destructiveIcon)}>
+          <CircleAlert />
+        </div>
+      ) : null}
     </label>
   );
 }
