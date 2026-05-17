485e2fa940e8ba2823fbaaa4d8f349ebf9e9c92e
Kirill Siukhin <kirillsiukhin@proton.me>
2026-05-18 03:07:19 +0500

    refactor: remove mock data and add group api

--- src/entities/group/api/index.ts
+++ src/entities/group/api/index.ts
@@ -1,22 +1,22 @@
+import { client } from '@/shared/api';
 import type { Group } from '../model/types';
 
-const academicGroups: Group[] = [
-  { name: '24-11', description: '' },
-  { name: '24-12', description: '' },
-  { name: '24-13', description: '' },
-  { name: '24-14', description: '' },
-];
-
-const otherGroups: Group[] = [
-  { name: 'A0', description: '' },
-  { name: 'A1', description: '' },
-  { name: 'A2', description: '' },
-  { name: 'B1', description: '' },
-  { name: 'B2', description: '' },
-  { name: 'C1', description: '' },
-];
-
 export const GroupApi = {
-  fetchAcademicGroups: async (): Promise<Group[]> => academicGroups,
-  fetchOtherGroups: async (): Promise<Group[]> => otherGroups,
+  fetchAcademicGroups: async (): Promise<Group[]> => {
+    try {
+      const res = await client.get<Group[]>('/groups/academic');
+      return res.data;
+    } catch {
+      throw new Error('UNKNOWN_ERROR');
+    }
+  },
+
+  fetchOtherGroups: async (): Promise<Group[]> => {
+    try {
+      const res = await client.get<Group[]>('/groups/other');
+      return res.data;
+    } catch {
+      throw new Error('UNKNOWN_ERROR');
+    }
+  },
 };
