2fb3dbbce345520cd4ef6b631321beca77643e40
Kirill Siukhin <kirillsiukhin@proton.me>
2026-05-18 03:04:15 +0500

    feat: users list in board

--- src/entities/user/api/index.ts
+++ src/entities/user/api/index.ts
@@ -1,13 +1,19 @@
-import { faker } from '@faker-js/faker';
 import type { FetchUsersRequest, User } from '../model/types';
-
-const users: User[] = Array.from({ length: 20 }, () => ({
-  id: crypto.randomUUID(),
-  fName: faker.person.firstName(),
-  lName: faker.person.lastName(),
-}));
+import { client } from '@/shared/api';
 
 export const UserApi = {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  fetchUsers: async (_data: FetchUsersRequest): Promise<User[]> => users,
+  fetchUsers: async (data: FetchUsersRequest): Promise<User[]> => {
+    try {
+      const res = await client.get('/groups/intersection', {
+        params: {
+          group1Cn: data.name1,
+          group2Cn: data.name2,
+        },
+      });
+
+      return res.data;
+    } catch {
+      throw new Error('UNKNOWN_ERROR');
+    }
+  },
 };
--- src/pages/board/ui/board-page.tsx
+++ src/pages/board/ui/board-page.tsx
@@ -1,11 +1,24 @@
 import classes from './board-page.module.css';
 import { BoardEntity } from '@/entities/board';
 import { CreateBoardDialog } from '@/features/board';
+import { Input } from '@/shared/ui/input';
+import { ChevronLeft } from 'lucide-react';
+import { useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 export function BoardPage() {
   const board = useSelector(BoardEntity.selectors.selectBoard);
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const users = useMemo(() => {
+    if (!board) return [];
+
+    const q = searchQuery.trim().toLowerCase();
+
+    return board.users.filter((u) => `${u.fName} ${u.lName}`.includes(q));
+  }, [board, searchQuery]);
 
   const navigate = useNavigate();
 
@@ -15,7 +28,25 @@ export function BoardPage() {
 
   return (
     <div className={classes.wrapper}>
-      <section className={classes.sideSection}></section>
+      <section className={classes.sideSection}>
+        <Link to="/" className={classes.homeLink}>
+          <ChevronLeft /> Вернуться на Главную
+        </Link>
+
+        <Input
+          placeholder="Найти"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <div className={classes.usersList} role="list">
+          {users.map((user) => (
+            <div key={user.id} className={classes.userItem} role="listitem">
+              {user.fName} {user.lName}
+            </div>
+          ))}
+        </div>
+      </section>
 
       <section className={classes.middleSection}></section>
 
